ca7900d8a3e3e4e788f0cfc416784266f9623ff8
Mark Arts <markarts@hoppinger.com>
2016-01-14 13:18:25 +0100

    Initial commit containing op 1 and part of op 2

---
+++ opdracht_1/Template/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using ImageUtilities;
+
+namespace Template
+{
+
+    public struct YUV
+    {
+        public float Y, U, V;
+
+        public YUV(float y, float u, float v)
+        {
+            this.Y = y;
+            this.U = u;
+            this.V = v;
+        }
+
+        public static YUV fromRGB(Color color)
+        {
+            float Wr = 0.299f;
+            float Wb = 0.114f;
+            float Wg = 1 - Wr - Wb;
+            float UMax = 0.436f;
+            float VMax = 0.615f;
+
+            float y = Wr * color.R + Wg * color.G + Wb * color.B;
+
+            return new YUV(
+                y, 
+                UMax * ( (color.B - y) / ( 1 - Wb) ),
+                VMax * ( (color.R - y) / (1 - Wb) )
+            );
+        }
+
+        public Color toRGBA()
+        {
+            float Wr = 0.299f;
+            float Wb = 0.114f;
+            float Wg = 1 - Wr - Wb;
+            float UMax = 0.436f;
+            float VMax = 0.615f;
+
+            return Color.FromArgb(
+                (byte) (Y + V * ((1 - Wr) / VMax) ),
+                (byte) (Y - U * ((Wb * (1 - Wb)) / (UMax * Wg)) ),
+                (byte) (Y + U * ((1 - Wb) / UMax) )
+            );
+        }
+    }
+
+
+    public class Program
+    {        
+        static void Main(string[] args)
+        {
+            Color[,] img = ImageUtilities.ImageViewer.LoadImage(@"C:\Users\Mark\Documents\CMIAAR01K\opdracht_1\Content\Content\forest.jpg");
+
+            Color[,] greyImg = MultiMap(img, c =>
+            {
+                int m = (c.R + c.G + c.B) / 3;
+                return Color.FromArgb(c.A, m, m, m);
+            });
+
+            YUV[,] yuvImg = MultiMap(img, YUV.fromRGB);          
+            YUV[,] lum = MultiMap(yuvImg, c => new YUV(c.Y, 0, 0) );
+            Color[,] lumRGB = MultiMap(lum, c => c.toRGBA());
+
+            // ImageUtilities.ImageViewer.DrawImagePair(greyImg, img);
+            // ImageUtilities.ImageViewer.DrawImagePair(MultiMap(yuvImg, c => c.toRGBA()), img);
+            // ImageUtilities.ImageViewer.DrawImagePair(lumRGB, img);
+            ImageUtilities.ImageViewer.DrawImagePair(lumRGB, greyImg);
+        }
+
+        static R[,] MultiMap<T, R>(T[,] colors, Func<T, R> f)
+        {
+            R[,] ret = new R[colors.GetLength(0), colors.GetLength(1)];
+            for (int x = 0; x < colors.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < colors.GetLength(1); y += 1)
+                {
+                    ret[x, y] = f(colors[x, y]);
+                }
+            }
+
+            return ret;
+        }
+
+    }
+}
---
+++ opdracht_2/Template/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using ImageUtilities;
+
+namespace Template
+{
+    public class Program
+    {        
+        public struct Resultion
+        {
+            public int x, y;
+            public Resultion(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Color[,] img = ImageUtilities.ImageViewer.LoadImage(@"C:\Users\Mark\Documents\CMIAAR01K\opdracht_1\Content\Content\forest.jpg");
+
+            Color[,] resImg = Resize(img, new Resultion(1920, 1080));
+            ImageUtilities.ImageViewer.DrawImagePair(resImg, img);
+        }
+
+        static Color[,] Resize(Color[,] img, Resultion res)
+        {
+            Color[,] resImg = new Color[res.x, res.y];
+
+            float xScale = (float)img.GetLength(0) / (float)res.x;
+            float yScale = (float)img.GetLength(1) / (float)res.y;            
+
+            resImg = MultiMap(resImg, (c, x, y) => img[(int)Math.Round(xScale * x), (int)Math.Round(yScale * y)] );
+
+            return resImg;
+        }
+
+        static R[,] MultiMap<T, R>(T[,] colors, Func<T, int, int, R> f)
+        {
+            R[,] ret = new R[colors.GetLength(0), colors.GetLength(1)];
+            for (int x = 0; x < colors.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < colors.GetLength(1); y += 1)
+                {
+                    ret[x, y] = f(colors[x, y], x, y );
+                }
+            }
+
+            return ret;
+        }
+
+    }
+}
