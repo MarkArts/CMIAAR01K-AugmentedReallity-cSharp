ba190206208fa38101557100767fe21d37caf113
MarkArts <markarts@hoppinger.com>
2016-01-14 14:30:44 +0100

    Adds bilinear scaling to exersice 2

--- opdracht_2/Template/Program.cs
+++ opdracht_2/Template/Program.cs
@@ -11,6 +11,13 @@ namespace Template
 {
     public class Program
     {        
+
+        public enum Method
+        {
+            Nearest,
+            Bilinear
+        }
+
         public struct Resultion
         {
             public int x, y;
@@ -23,22 +30,62 @@ namespace Template
 
         static void Main(string[] args)
         {
-            Color[,] img = ImageUtilities.ImageViewer.LoadImage(@"C:\Users\Mark\Documents\CMIAAR01K\opdracht_1\Content\Content\forest.jpg");
+            Color[,] img = ImageUtilities.ImageViewer.LoadImage(@"E:\Documents\Visual Studio 2015\Projects\CMIAAR01K-AugmentedReallity-cSharp\opdracht_2\Content\Content\minion.jpg");
 
-            Color[,] resImg = Resize(img, new Resultion(1920, 1080));
-            ImageUtilities.ImageViewer.DrawImagePair(resImg, img);
+            Color[,] nearImg = Resize(img, new Resultion(1920*2, 1080*2), Method.Nearest);
+            Color[,] BilImg = Resize(img, new Resultion(1920*2, 1080*2), Method.Bilinear);
+            ImageUtilities.ImageViewer.DrawImagePair(nearImg, BilImg);
         }
 
-        static Color[,] Resize(Color[,] img, Resultion res)
+        static Color[,] Resize(Color[,] img, Resultion res, Method method)
         {
             Color[,] resImg = new Color[res.x, res.y];
 
             float xScale = (float)img.GetLength(0) / (float)res.x;
-            float yScale = (float)img.GetLength(1) / (float)res.y;            
+            float yScale = (float)img.GetLength(1) / (float)res.y;
 
-            resImg = MultiMap(resImg, (c, x, y) => img[(int)Math.Round(xScale * x), (int)Math.Round(yScale * y)] );
+            int boundX = img.GetLength(0) - 1;
+            int boundY = img.GetLength(1) - 1;
+
+            if (method == Method.Nearest)
+            {
+                resImg = MultiMap(resImg, (c, x, y) => img[(int)clamp((float)Math.Round(xScale * x), 0, boundX), (int)clamp((float)Math.Round(yScale * y), 0, boundY)]);
+            }else if(method == Method.Bilinear)
+            {
+                resImg = MultiMap(resImg, (c, x, y) =>
+                {
+                    float nX = xScale * x;
+                    float nY = yScale * y;
+
+                    float factorX = nX - (float)Math.Truncate(nX);
+                    float factorY = nY - (float)Math.Truncate(nY);
+
+                    Color t1 = img[(int)clamp((float)Math.Floor(nX),0, boundX), (int)clamp((float)Math.Floor(nY),0,boundY)];
+                    Color t2 = img[(int)clamp((float)Math.Ceiling(nX), 0, boundX), (int)clamp((float)Math.Floor(nY), 0, boundY)];
+                    Color b1 = img[(int)clamp((float)Math.Floor(nX), 0, boundX), (int)clamp((float)Math.Ceiling(nY), 0, boundY)];
+                    Color b2 = img[(int)clamp((float)Math.Ceiling(nX), 0, boundX), (int)clamp((float)Math.Ceiling(nY), 0, boundY)];
+
+                    return Color.FromArgb(
+                        (int)lerp(lerp(t1.R, t2.R, factorX), lerp(b1.R, b2.R, factorX), factorY),
+                        (int)lerp(lerp(t1.G, t2.G, factorX), lerp(b1.G, b2.G, factorX), factorY),
+                        (int)lerp(lerp(t1.B, t2.B, factorX), lerp(b1.B, b2.B, factorX), factorY)
+                    );
+                });
+            }
 
             return resImg;
+        }
+
+
+        static float lerp(float value1, float value2, float amount)
+        {
+            return value1 + (value2 - value1) * amount;
+        }
+        static float clamp(float val, float min, float max)
+        {
+            if (val < min) return min;
+            if (val > max) return max;
+            return val;
         }
 
         static R[,] MultiMap<T, R>(T[,] colors, Func<T, int, int, R> f)
